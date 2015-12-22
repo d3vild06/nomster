cdc56f6727de5117e285728a318a6dfeb002207f
Roberto Q <sgtquezada@gmail.com>
2015-12-22 17:28:55 +0000

    added picture uploader and images to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,8 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.page(params[:page]).per(4)
+    @places = Place.page(params[:page]).per(6)
+    @photos = Photo.all
   end
   
   def new
@@ -21,6 +22,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
   
   def edit
